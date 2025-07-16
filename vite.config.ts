00e5262b27a1d8d37f2ab0c5036ec43436493e31
srushtimadalgi <155770064+srushtimadalgi@users.noreply.github.com>
2025-07-16 14:55:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
+    mode === 'development',
   ].filter(Boolean),
   resolve: {
     alias: {
